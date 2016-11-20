9c0c2b4b18e75ed93948067d379e0d50eca6bcf8
Shanmugam Rathinasamy <shaninfoway@yahoo.co.in>
2016-11-19 19:31:49 -0500

    Initial project setup

---
+++ rws/src/test/java/com/cognizantminds/rws/RwsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cognizantminds.rws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RwsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
